280bd62b4d90269157285395fca9c80284360807
Krishna kushvanshi <68548987+krishna2808@users.noreply.github.com>
2021-03-07 06:03:48 +0000

    Delete Chef and Groups.py

--- Chef and Groups.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    s,count  = input(), 0
-    if(s[0] == '1'):
-        count += 1 
-    for i in range(1,len(s)):
-        if(s[i]=='1' and s[i] != s[i-1]):
-            count += 1 
-    print(count)        
-            
